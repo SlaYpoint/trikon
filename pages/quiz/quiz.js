0927c8db1c8f75605a9cf5c9909a848f53f61001
SlaYpoint <codersaysady@gmail.com>
2021-08-28 01:11:40 +0530

    feat : trikone quiz

---
+++ pages/quiz/quiz.js
@@ -0,0 +1,27 @@
+const form = document.forms[0];
+const submitBtn = document.querySelector("#submit");
+const quesDivs = document.querySelectorAll(".radio");
+const output = document.querySelector("#result");
+
+let score = 0;
+const rightAns = ["opt2", "opt1", "opt3", "opt2", "opt1", "opt1"];
+
+form.addEventListener('submit', (e) => {
+    e.preventDefault();
+
+    const formData = new FormData(form);
+    let idx = 0;
+
+    for (let inp of formData) {
+        if (inp[1] === rightAns[idx]) {
+            quesDivs[idx].style.background = '#A7F3D0';
+            score++;
+        } else {
+            quesDivs[idx].style.background = "#FCA5A5";
+        }
+        idx++;
+    }
+
+    submitBtn.style.display = "none";
+    output.innerText = score;
+});
